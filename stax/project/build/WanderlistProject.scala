4b2567c09ec0853a7ebc83afc18cd72b00ecbdf6
Jorge Ortiz <jortiz@jortiz-mn.local>
2010-03-15 00:52:52 -0700

    Remove comments

--- stax/project/build/WanderlistProject.scala
+++ stax/project/build/WanderlistProject.scala
@@ -12,7 +12,7 @@ class WanderlistProject(info: ProjectInfo) extends DefaultWebProject(info) with 
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
   val derby = "org.apache.derby" % "derby" % "10.2.2.0" % "runtime"
-  val mysql = "mysql" % "mysql-connector-java" % "5.1.6" % "runtime" // from "http://mvn.stax.net/content/repositories/public"
+  val mysql = "mysql" % "mysql-connector-java" % "5.1.6" % "runtime"
   val junit = "junit" % "junit" % "3.8.1" % "test"
 
   // Repositories
