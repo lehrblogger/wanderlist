f850d28d9408a3583e33392e01dde71049ae048e
Steven Lehrburger <memento85s@gmail.com>
2010-04-04 21:52:41 -0400

    removed old Service and Provider files

--- stax/src/main/scala/wanderlist/model/Contact.scala
+++ stax/src/main/scala/wanderlist/model/Contact.scala
@@ -16,6 +16,9 @@ class Contact extends LongKeyedMapper[Contact] with IdPK {
 }
 object Contact extends Contact with LongKeyedMetaMapper[Contact] {}
 
+
+
+
 class ContactGroup extends LongKeyedMapper[ContactGroup] with IdPK {
     def getSingleton = ContactGroup
     object contact extends MappedLongForeignKey(this, Contact)
@@ -26,3 +29,27 @@ object ContactGroup extends ContactGroup with LongKeyedMetaMapper[ContactGroup] 
         //TODO validate that Contact and Group have same owner
         this.create.contact(c).group(g).save
 }
+
+
+
+class HotpotatoServiceContact extends LongKeyedMapper[HotpotatoServiceContact] with IdPK {
+    def getSingleton = HotpotatoServiceContact
+    object service extends MappedLongForeignKey(this, HotpotatoService)
+    object contact extends MappedLongForeignKey(this, Contact)
+}
+object HotpotatoServiceContact extends HotpotatoServiceContact with LongKeyedMetaMapper[HotpotatoServiceContact] {
+    def join (s: HotpotatoService, c: Contact) = 
+        this.create.service(s).contact(c).save
+}
+
+class FoursquareServiceContact extends LongKeyedMapper[FoursquareServiceContact] with IdPK {
+    def getSingleton = FoursquareServiceContact
+    object service extends MappedLongForeignKey(this, FoursquareService)
+    object contact extends MappedLongForeignKey(this, Contact)
+}
+object FoursquareServiceContact extends FoursquareServiceContact with LongKeyedMetaMapper[FoursquareServiceContact] {
+    def join (s: FoursquareService, c: Contact) = 
+        this.create.service(s).contact(c).save
+}
+
+
